57fd3b2e2c850368f0fad232c280fe0b5b97fc77
aniketgaikwad4635 <aniketgaikwad4635@gmail.com>
2025-02-19 21:26:42 +0530

    developer3 first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String args[]){
         System.out.println("hello");
-        developer1Method();
+        developer3Method();
     }
 
-    public static void developer1Method(){
-        System.out.println("developer1Method");
+    public static void developer3Method(){
+        System.out.println("developer3Method");
     }
 }
